fbf9ccc045ecceaa19346b7f208d3bb3b45724ab
Andrey <luffytaroyo@gmail.com>
2023-03-13 13:46:13 +0300

    add timer project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const burgerBtn = document.querySelector('#burger');
 const header = document.querySelector('.header');
 
+
+const body = document.body;
+
+
 const openProjectBtn = document.querySelector('.about__projects');
 const projects = document.querySelector('.projects__container');
 
@@ -8,6 +12,7 @@ const closeProjectsBtn = document.querySelector('.project__close');
 
 burgerBtn.addEventListener('click', () => {
     header.classList.toggle('open');
+    body.classList.toggle('no__scroll');
 });
 
 
@@ -17,4 +22,21 @@ openProjectBtn.addEventListener('click', () => {
 
 closeProjectsBtn.addEventListener('click', () => {
     projects.classList.remove('project__open');
+});
+
+//scroll
+
+const aboutLinks = document.querySelectorAll(".menu__link");
+
+aboutLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    header.classList.remove("open");
+    body.classList.remove('no__scroll');
+    const blockID = link.getAttribute("href").substr(1);
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
 });
