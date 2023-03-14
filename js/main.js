673ddc0d50c5e80c9ed09f7cf94fdd8c453802bd
Andrey <luffytaroyo@gmail.com>
2023-03-14 16:20:57 +0300

    hello

--- js/main.js
+++ js/main.js
@@ -10,11 +10,17 @@ const projects = document.querySelector('.projects__container');
 
 const closeProjectsBtn = document.querySelector('.project__close');
 
+
+//burger menu
+
 burgerBtn.addEventListener('click', () => {
     header.classList.toggle('open');
     body.classList.toggle('no__scroll');
 });
 
+
+
+//projects
 
 openProjectBtn.addEventListener('click', () => {
     projects.classList.add('project__open');
@@ -40,3 +46,31 @@ aboutLinks.forEach((link) => {
     });
   });
 });
+
+//scroll
+
+window.addEventListener("scroll", function () {
+  let scrollToTopButton = document.querySelector(".scroll-to-top");
+  if (window.pageYOffset > 100) {
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+});
+
+document
+  .querySelector(".scroll-to-top")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    let scrollDuration = 300;
+    let scrollStep = -window.scrollY / (scrollDuration / 15);
+    let scrollInterval = setInterval(function () {
+      if (window.scrollY != 0) {
+        window.scrollBy(0, scrollStep);
+      } else clearInterval(scrollInterval);
+    }, 15);
+  });
+
+
+  // email form
+
